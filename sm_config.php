e11bf3dd2921f41255b1a0f260ec662fc7ba7151
xurenlu <xurenlu@gmail.com>
2011-05-23 11:12:55 +0800

    add table_prefix support

--- sm_config.php
+++ sm_config.php
@@ -1,8 +1,9 @@
 <?php
 /** sm_config,sm_temp,sm_data 是整个框架依赖的三个全局变量,sm_config 用来设置全局配置;sm_temp是中间变量,做一些处理工作, sm_data 用来从controller中传递数据到viewer来处理输出;  */
 global $sm_config,$sm_temp,$sm_data;
-$sm_config["mysql"][0]=array("host"=>"localhost","user"=>"root","password"=>"","database"=>"game");
-$sm_config["mysql"][1]=array("host"=>"localhost","user"=>"root","password"=>"","database"=>"game");
+$sm_config["mysql"][0]=array("host"=>"localhost","user"=>"root","password"=>"","database"=>"codeanycms");
+$sm_config["mysql"][1]= $sm_config["mysql"][0];
+$sm_config["table_prefix"]="codeany_";
 $sm_config["prepare_sql"]="set names utf8";
 $sm_config["memcache"]["group_1"]=
     array(
@@ -10,31 +11,28 @@ $sm_config["memcache"]["group_1"]=
         array("host"=>"localhost","port"=>11211)
     );
 $sm_config["debug"]=true;
-$sm_config["sql_debug"]=true;
-$sm_config["pagesize"]=5;
-$sm_config["app_root"]=dirname(__FILE__);
+$sm_config["sql_debug"]=false;
+$sm_config["pagesize"]=20;
+$sm_config["app_root"]=dirname(dirname(__FILE__));
 $sm_config["encrypt_secret"]='832-832phfdsf8y4idfjer[iuyutrsedhjery874knr9342[gof]f@$%YE$#^*U*(^U#^&GHGFU*(JGRUo{PIU*%$IKJH6734iu734khgfdsfghqewjfdttyiupojunfrkjgttuikojrrds345yjuj234578ijhgdee3yuio9632shj;,gt5dxfyhjikl,tedgvnkop';
 $sm_config["url_routes"]=
     array(
-        "{controller}/{action}-{id}-{page}",
-        "{controller}/{action}-p-{page}",
-        "{controller}/{action}/{id}-{cate}-{page}.{format}"=>
-        array(
-            "controller"=>"([^.^\/]*)",
-            "action"=>"([^.]*)"
-        ),
-        "{controller}/{action}/{id}.{format}",
+        "{controller}/{action}/p_{page}\.{format}",
+        "{controller}/{action}/p_{page}",
+        "{controller}/{action}/{id}-p-{page}\.{format}",
+        "{controller}/{action}/{id}\.{format}",
         "{controller}/{action}/{id}",
+        "{controller}/{action}\.{format}",
         "{controller}/{action}",
-        "{id}.{format}",
-        "{file}"
+        "{controller}",
     );
-$sm_config["url_namespace"]="/admin/";
 $sm_config["url_maps"]=
     array(
-        'post' => 'article/add/',
-        'v-' => 'question/view/',
-        'show-' => 'article/view/',
-        'c-' => 'comment/view/'
-    );
-
+        'v-' => 'questionfront/view/',
+        'show-' => 'articlefront/view/pagename_article_view/',
+        'c-' => 'commentfront/view/'
+    );  
+$sm_config["url_namespace"]=($_SERVER["SCRIPT_NAME"])."/";
+$sm_config["use_layout"]=true;
+$sm_config["LC_ALL"]="zh_CN";
+define("PREFIX",$sm_config["table_prefix"]);
